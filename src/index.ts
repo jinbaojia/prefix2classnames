9c1c49aefb9424968d17464b59e3c238716624b7
bjjin <17856419249@163.com>
2022-08-29 15:37:50 +0800

    调整

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,56 @@
-export { classnames as default } from './classnames';
+import { addPrefix, isPlainObject, isValidString } from './tools.js';
+
+type FalseArg = undefined | null | false | 0;
+type Args = Array<
+  FalseArg | string | { '-'?: string; [propName: string]: any } | Args
+>;
+
+export const Prefix = '-';
+
+function classnamesImpl(args: Args, classList: string[]): string {
+  let localPrefix = '';
+  args.forEach((arg) => {
+    if (!arg) {
+      return;
+    }
+    if (Array.isArray(arg)) {
+      const _localPrefix = classnamesImpl(arg, classList);
+      !localPrefix && (localPrefix = _localPrefix);
+    } else if (isPlainObject(arg)) {
+      const keys = Object.keys(arg);
+      if (keys.length === 0) {
+        return;
+      }
+      const prefix = arg[Prefix];
+      if (isValidString(prefix)) {
+        localPrefix = prefix;
+      }
+      const list = keys.filter(
+        (key) => arg[key as keyof typeof arg] && key !== Prefix
+      );
+      classList.push(...list);
+    } else if (typeof arg === 'string') {
+      classList.push(arg);
+    }
+  });
+  return localPrefix;
+}
+
+export default function classNames(this: any, ...args: Args): string {
+  if (args.length === 0) {
+    return '';
+  }
+  let classList: string[] = [];
+  const localPrefix = classnamesImpl(args, classList);
+  classList = [...new Set(classList)];
+  addPrefix(classList, localPrefix);
+  if (this) {
+    const { [Prefix]: globalPrefix } = this;
+    addPrefix(classList, globalPrefix);
+    const stylesLen = Object.keys(this).length;
+    if (stylesLen > 0 && !(stylesLen === 1 && this[Prefix])) {
+      classList = classList.map((key) => this[key] || key);
+    }
+  }
+  return classList.join(' ');
+}
